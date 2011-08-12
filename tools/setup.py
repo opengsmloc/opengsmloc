149521915aa5c810305382f2fcd6903cae28612b
Brendan Johan Lee <brendan@vanntett.net>
2011-08-12 21:28:24 +0200

    Created initial setup.py

---
+++ tools/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+TODO: Description and Lisence
+
+@author: Brendan Johan Lee
+@contact: brendan@vanntett.net
+@version: 1.0
+"""
+from distutils.core import setup
+
+setup(name = 'opengsmloc tools',
+      version = '0.1',
+      description = 'Useful tools not related to the core backend or clients. See README for list and discription of tools',
+      author = 'Brendan Johan Lee',
+      author_email = 'brendan@vanntett.net',
+      url = 'http://opengsmloc.org',
+      scripts = ['glocalizer'])
